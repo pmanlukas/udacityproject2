e0259341419c91cf8ad1683d39febb7271b8a243
pmanlukas <lukas.pollmann@tum.de>
2019-06-03 17:50:13 -0400

    * added template for problems 4,5 and 6

---
+++ problem_5.py
@@ -0,0 +1,53 @@
+import hashlib
+
+class Block:
+
+    def __init__(self, timestamp, data, previous_hash):
+      self.timestamp = timestamp
+      self.data = data
+      self.previous_hash = previous_hash
+      self.hash = self.calc_hash()
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def __str__(self):
+        cur_head = self.head
+        out_string = ""
+        while cur_head:
+            out_string += str(cur_head.value) + " -> "
+            cur_head = cur_head.next
+        return out_string
+
+
+    def append(self, value):
+
+        if self.head is None:
+            self.head = Node(value)
+            return
+
+        node = self.head
+        while node.next:
+            node = node.next
+
+        node.next = Node(value)
+
+    def size(self):
+        size = 0
+        node = self.head
+        while node:
+            size += 1
+            node = node.next
+
+        return size
+
+def calc_hash(self):
+      sha = hashlib.sha256()
+
+      hash_str = "We are going to encode this string of data!".encode('utf-8')
+
+      sha.update(hash_str)
+
+      return sha.hexdigest()
+
