5aa538f60422f40811c03f5db992116e3bd5274d
pmanlukas <lukas.pollmann@tum.de>
2019-06-10 14:45:58 +0200

    * add error handling and cases for problem 4

--- problem_4.py
+++ problem_4.py
@@ -39,6 +39,12 @@ def is_user_in_group(user, group):
         user(str): user name/id
         group(class:Group): group to check user membership against
     """
+    if isinstance(group, str):
+        return "Group must be instance of group"
+
+    if user == group:
+        return "Error: can't pass group as User"
+
     if user == group.get_name():
         return True
     if user in group.get_users():
@@ -47,7 +53,15 @@ def is_user_in_group(user, group):
         return is_user_in_group(user, grp)
     return False
 
-
+#Case 1
 print(is_user_in_group("child", child))  # True
 print(is_user_in_group("", child))  # False
 print(is_user_in_group("sub_child_user", parent))  # True
+
+#Case 2
+print(is_user_in_group("child", "child")) # group must be instance of group
+
+#Case 3
+print(is_user_in_group(child, child)) # Error: can't pass group as User
+
+
