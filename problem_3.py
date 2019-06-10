554748bc4bf8d247e6eebdbca4509f5a8d351bb1
pmanlukas <lukas.pollmann@tum.de>
2019-06-10 14:52:22 +0200

    * add more testcase for problem 3

--- problem_3.py
+++ problem_3.py
@@ -97,3 +97,23 @@ if __name__ == "__main__":
     print("The size of the decoded data is: {}\n".format(
         sys.getsizeof(decoded_data)))
     print("The content of the encoded data is: {}\n".format(decoded_data))
+
+#Case 4
+
+    a_great_sentence = "AAAAAAA"
+
+    print("The size of the data is: {}\n".format(
+        sys.getsizeof(a_great_sentence)))
+    print("The content of the data is: {}\n".format(a_great_sentence))
+
+    encoded_data, tree = huffman_encoding(a_great_sentence)
+
+    print("The size of the encoded data is: {}\n".format(
+        sys.getsizeof(int(encoded_data, base=2))))
+    print("The content of the encoded data is: {}\n".format(encoded_data))
+
+    decoded_data = huffman_decoding(encoded_data, tree)
+
+    print("The size of the decoded data is: {}\n".format(
+        sys.getsizeof(decoded_data)))
+    print("The content of the encoded data is: {}\n".format(decoded_data))
