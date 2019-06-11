4f9e831c05c7048b011f633bf7c35d3c8d0d21e3
pmanlukas <lukas.pollmann@tum.de>
2019-06-11 22:59:09 +0200

    * add error handling

--- problem_1.py
+++ problem_1.py
@@ -7,23 +7,30 @@ class LRU_Cache(object):
         self.capacity = capacity
         self.cache = collections.OrderedDict()
 
-    def get(self, key):
+    def get(self, key=None):
         # Retrieve item from provided key. Return -1 if nonexistent.
         try:
+            if key == None:
+                return "Key can't be empty"
             value = self.cache.pop(key)
             self.cache[key] = value
             return value
         except KeyError:
             return -1
 
-    def set(self, key, value):
+    def set(self, key, value=None):
         # Set the value if the key is not present in the cache. If the cache is at capacity remove the oldest item.
-        try:
-            self.cache.pop(key)
-        except KeyError:
-            if len(self.cache) >= self.capacity:
-                self.cache.popitem(last=False)
-        self.cache[key] = value
+        if value is None:
+            print("Value can't be empty")
+            return
+        else:
+            try:
+                
+                self.cache.pop(key)
+            except KeyError:
+                if len(self.cache) >= self.capacity:
+                    self.cache.popitem(last=False)
+            self.cache[key] = value
 
 
 # Case 1
